5aaa953f038631704b87d32eea466b1c88a2f6ca
dmclark53 <dmclark5@gmail.com>
2016-07-19 21:03:04 -0700

    model training and scoring

---
+++ wine_quality/model.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+
+# Softmax Regression Model
+def softmax_regression(x):
+    W = tf.Variable(tf.zeros([10,2]), name="W")
+    b = tf.Variable(tf.zeros([2]), name="b")
+    y = tf.nn.softmax(tf.matmul(x,W) + b)
+    return y, [W, b]
